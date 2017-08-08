cd11916516344256db9417f812ae721dad324761
Shusen Wang <wssatzju@gmail.com>
2017-08-08 11:54:25 -0700

    initial commit

---
+++ src/main/scala/svd/SparkSvd.scala
@@ -0,0 +1,22 @@
+package alchemist.test.svd
+
+// spark-core
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd._
+// spark-sql
+import org.apache.spark.sql.SparkSession
+// others
+import scala.math
+// breeze
+import breeze.linalg._
+import breeze.numerics._
+
+
+object SparkSvd {
+    def main(args: Array[String]) {
+        
+    }
+    
+}
