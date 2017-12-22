cbc19d7d388a485656c2a7a5557fed53b8ada8b4
Shusen Wang <wssatzju@gmail.com>
2017-12-22 10:11:02 -0800

    test TIMIT

--- bash/run_local_ClassificationRFM_spark.sh
+++ bash/run_local_ClassificationRFM_spark.sh
@@ -5,13 +5,14 @@
 PROJ_HOME="$HOME/Code/TestAlchemist"
 NUM_SPLIT="1000"
 NUM_FEATURE="1000"
-REG_PARAM='1E-8'
+REG_PARAM='1E-4'
 MASTER="local["$NUM_SPLIT"]"
 
 # the rest does not need change
 
 # data
-DATA_FILE="$PROJ_HOME/data/mnist.scale.t"
+# DATA_FILE="$PROJ_HOME/data/mnist.scale.t"
+DATA_FILE=$PROJ_HOME"/data/timit-test.csv"
 
 # .jar file
 JAR_FILE="$PROJ_HOME/target/scala-2.11/testalchemist_2.11-1.0.jar"
--- src/main/scala/regression/SparkRfmClassification.scala
+++ src/main/scala/regression/SparkRfmClassification.scala
@@ -27,7 +27,7 @@ object SparkRfmClassification {
         val numFeatures: Int = args(1).toInt
         val gamma: Double = args(2).toDouble
         val numSplits: Int = 100//args(3).toInt
-        val numClass: Int = 10
+        val numClass: Int = 147 //10
         
         //// Launch Spark
         var t1 = System.nanoTime()
@@ -43,11 +43,12 @@ object SparkRfmClassification {
         
         //// Load data and perform RFM
         val rddRaw: RDD[(Int, Array[Double])] = SparkRfmClassification
-                                                    .loadLibsvmData(spark, filepath, numSplits)
+                                                    //.loadLibsvmData(spark, filepath, numSplits)
+                                                    .loadCsvData(spark, filepath, numSplits)
                                                     .persist()
-        val rddRfm: RDD[(Int, Array[Double])] = SparkRfmClassification
-                                                    .randomFeatureMap(rddRaw, numFeatures)
-                                                    .persist()
+        //val rddRfm: RDD[(Int, Array[Double])] = SparkRfmClassification
+        //                                            .randomFeatureMap(rddRaw, numFeatures)
+        //                                            .persist()
         
         
         def oneHotEncode(y: Int): Array[Double] = {
@@ -55,26 +56,39 @@ object SparkRfmClassification {
             yArray(y) = 1
             yArray
         }
-        val rddOneHot: RDD[(Array[Double], Array[Double])] = rddRfm.map(pair => (oneHotEncode(pair._1), pair._2))
+        val rddOneHot: RDD[(Array[Double], Array[Double])] = rddRaw.map(pair => (oneHotEncode(pair._1), pair._2)).persist()
         
+        println(rddOneHot.count)
+        println(rddOneHot.take(1)(0)._1.mkString(" "))
+        println(rddOneHot.take(1)(0)._2.mkString(" "))
         
         //// Train ridge regression by CG
         val cg: CgMultiTask.Driver = new CgMultiTask.Driver(sc, rddOneHot)
         var maxIter: Int = 150
         cg.train(gamma, maxIter)
         cg.trainMisclassify()
-        /*
-        var results: (Array[Double], Array[Double], Array[Double]) = cg.train(gamma, maxIter)
-        println("\n ")
-        println("====================================================================")
-        println("CG (gamma=" + gamma.toString + ", MaxIter=" + maxIter.toString + ")")
-        println("\n ")
-        println("Objective Value\t Training Error\t Elapsed Time")
-        results.zipped.foreach(this.printAsTable)
-        */
-        
         spark.stop
     }
+    
+    def loadCsvData(spark: SparkSession, filepath: String, numSplits: Int): RDD[(Int, Array[Double])] = {
+        //// Load data from file
+        val t1 = System.nanoTime()
+        val df = spark.read.format("csv").load(filepath)
+        val rdd: RDD[Array[Double]] = df.rdd
+                .map(vec => Vectors.parse(vec.toString).toArray)
+                .persist()
+        //val n: Long = rdd.count()
+        val d: Int = rdd.take(1)(0).size
+        //println("n = " + n.toString + ", d = " + d.toString)
+        val rdd2: RDD[(Int, Array[Double])] = rdd.map(arr => (arr(0).toInt-1, arr.slice(1, d))).persist()
+        rdd2.count()
+        var t2 = System.nanoTime()
+        println("Time cost of loading data is " + ((t2 - t1) * 1.0E-9).toString)
+        println(" ")
+
+        rdd2
+    }
+    
     
     def loadLibsvmData(spark: SparkSession, filepath: String, numSplits: Int): RDD[(Int, Array[Double])] = {
         //// Load data from file
@@ -95,10 +109,10 @@ object SparkRfmClassification {
     def randomFeatureMap(rdd: RDD[(Int, Array[Double])], numFeatures: Int): RDD[(Int, Array[Double])] = {
         val rdd2: RDD[(Double, Array[Double])] = rdd.map(pair => (pair._1.toDouble, pair._2)).persist()
         //// estimate the kernel parameter (if it is unknown)
-        //var sigma: Double = rdd2.glom.map(Kernel.estimateSigma).mean
-        //sigma = math.sqrt(sigma)
-        //println("Estimated sigma is " + sigma.toString)
-        var sigma: Double = 10.16 // MNIST
+        var sigma: Double = rdd2.glom.map(Kernel.estimateSigma).mean
+        sigma = math.sqrt(sigma)
+        println("Estimated sigma is " + sigma.toString)
+        //var sigma: Double = 10.16 // MNIST
         
         
         //// Random feature mapping
